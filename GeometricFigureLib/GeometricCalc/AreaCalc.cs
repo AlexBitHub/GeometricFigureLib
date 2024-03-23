64355b5d53c870a2774d1117b3c7b69d4b0cbc0a
AlexeyBit <abulyg1n@yandex.ru>
2024-03-23 17:14:33 +0300

    Создание классов Circle и Triangle и реализация расчета площади фигур

---
+++ GeometricFigureLib/GeometricCalc/AreaCalc.cs
@@ -0,0 +1,12 @@
+﻿using GeometricFigureLib.GeometricTypes;
+
+namespace GeometricFigureLib.GeometricCalc
+{
+    public static class AreaCalc
+    {
+        public static double GetFigureArea(IFigure figure)
+        {
+            return figure.GetArea();
+        }
+    }
+}
---
+++ GeometricFigureLib/GeometricTypes/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigureLib.GeometricTypes
+{
+    public class Circle : IFigure
+    {
+        private double _radius;
+        public Circle(double radius) 
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус круга не может быть меньше или равен нулю.");
+            }
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.PI * _radius;
+        }
+    }
+}
