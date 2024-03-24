156856c434152f4db8350f5363c4a7b810cdb911
AlexeyBit <abulyg1n@yandex.ru>
2024-03-24 19:30:23 +0300

    Разработка юнит тестов

---
+++ GeometricFigureLib.Test/CirlceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigureLib.Test
+{
+    [TestFixture]
+    public class CirlceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCaseSource(nameof(ZeroOrNegativeCirlceRadius))]
+        public void ZeroOrNegaitveCircleSidesTest(double radius)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Circle(radius);
+            });
+
+        }
+
+        public static object[] ZeroOrNegativeCirlceRadius =
+        {
+            -100, -24.5, 0
+        };
+    }
+}
