3fec2074bd1d8399ad04fce31ac72a0430ee7039
AlexeyBit <abulyg1n@yandex.ru>
2024-03-25 22:14:22 +0300

    SQL запрос для выбора всех продуктов и категорий

---
+++ QueryProductCategories.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE Products 
+(
+	Id INT PRIMARY KEY,
+	Name TEXT,
+  	CategoryId INT NULL
+);
+
+CREATE TABLE Categories 
+(
+	Id INT PRIMARY KEY,
+	Name TEXT
+);
+
+SELECT Products.Name, Categories.Name
+FROM Products
+LEFT JOIN Categories ON Products.CategoryId=Categories.Id;
